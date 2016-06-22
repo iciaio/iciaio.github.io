ecf86932449d338cc2ebfea90afc94a3052d4bf5
Alicia Iott <aliciamiott@gmail.com>
2016-06-22 17:24:12 -0400

    made new update function with request animation frame, no more settimeout, broke up draw into update, computeimage, updatebackground model

--- background_deletion.js
+++ background_deletion.js
@@ -1,34 +1,34 @@
 /*
 Credit for background subtration algorithm to: 
 
-   BACKGROUND SUBTRACTION USING A PIXEL-WISE
-   ADAPTIVE LEARNING RATE
-   FOR OBJECT TRACKING INITIALIZATION
-   Ka Ki Ng and Edward J. Delp
-   Video and Image Processing Laboratories (VIPER)
-   School of Electrical and Computer Engineering
-   Purdue University
-   West Lafayette, Indiana USA
+  BACKGROUND SUBTRACTION USING A PIXEL-WISE
+  ADAPTIVE LEARNING RATE
+  FOR OBJECT TRACKING INITIALIZATION
+  Ka Ki Ng and Edward J. Delp
+  Video and Image Processing Laboratories (VIPER)
+  School of Electrical and Computer Engineering
+  Purdue University
+  West Lafayette, Indiana USA
 
 Basic algorithm:
 1) BACKGROUND DETECTION AND SUBTRACTION
-   Using several (default 100) initial frames of the background, 
-   we use intensity and color differences between current frames 
-   and initial background frame averages to infer the foreground. 
+  Using several (default 100) initial frames of the background, 
+  we use intensity and color differences between current frames 
+  and initial background frame averages to infer the foreground. 
 
-   I modified the algorithm described in the above paper to include 
-   color differences as this algorithm will mainly be used to detect 
-   hands.
+  I modified the algorithm described in the above paper to include 
+  color differences as this algorithm will mainly be used to detect 
+  hands.
 
 2) UPDATING BACKGROUND MODEL
-   to account for movement/slight changes in the background we weight 
-   the probability of a pixel being a background pixel by the temporal 
-   duration of the backgorund pixel. ie if it doesnt stay in the 
-   foreground for very long, it is probably a background pixel.
+  to account for movement/slight changes in the background we weight 
+  the probability of a pixel being a background pixel by the temporal 
+  duration of the backgorund pixel. ie if it doesnt stay in the 
+  foreground for very long, it is probably a background pixel.
 
 3) To do later, time permitting: SUDDEN ILLUMINATION CHANGE AND SHADING MODEL
-   to account for lighting changes/camera auto exposure adjustments etc.
-   this can also be accounted for largely by disabling autoexposure on webcam though
+  to account for lighting changes/camera auto exposure adjustments etc.
+  this can also be accounted for largely by disabling autoexposure on webcam though
 */
 
 //this array will contain color and intensity data for each pixel averaged over the inital background frames
@@ -59,152 +59,185 @@ var cthreshold = 50;
 //number of background frames capture in beginning
 var numBackFrames = 100;
 
+//image data and pixel array within image data respectively
+var idata; 
+var data;
+
 //capture background gets average background
 function captureBackground() {
 
-   //get data from stream
-   var background = backContext.getImageData(0, 0, cw, ch);
-   var backgroundData = background.data;
-   backgroundAvg = new Array(backgroundData.length);
-   temporalBCG = new Array(backgroundData.length).fill(0);
+  //get data from stream
+  var background = backContext.getImageData(0, 0, cw, ch);
+  var backgroundData = background.data;
+  backgroundAvg = new Array(backgroundData.length);
+  temporalBCG = new Array(backgroundData.length).fill(0);
 
-   for (var j = 0; j < numBackFrames; j++){
+  for (var j = 0; j < numBackFrames; j++){
 
-      backContext.drawImage(video, 0, 0, cw, ch);
-      background = backContext.getImageData(0, 0, cw, ch);
-      backgroundData = background.data;
+    backContext.drawImage(video, 0, 0, cw, ch);
+    background = backContext.getImageData(0, 0, cw, ch);
+    backgroundData = background.data;
 
-      for(var i = 0; i < backgroundData.length ; i+=4) {
-         var r = backgroundData[i];
-         var g = backgroundData[i+1];
-         var b = backgroundData[i+2];
-         var brightness = (3*r+4*g+b)>>>3;
-         if (backgroundAvg[i] === undefined){
-            backgroundAvg[i] = r;
-         } else {
-            backgroundAvg[i] += r;
-         }
-         if (backgroundAvg[i+1] === undefined){
-            backgroundAvg[i+1] = g;
-         } else {
-            backgroundAvg[i+1] += g;
-         }
-         if (backgroundAvg[i+2] === undefined){
-            backgroundAvg[i+2] = b;
-         } else {
-            backgroundAvg[i+2] += b;
-         }
-         if (backgroundAvg[i+3] === undefined){
-            backgroundAvg[i+3] = brightness;
-         } else {
-            backgroundAvg[i+3] += brightness;
-         }
+    for(var i = 0; i < backgroundData.length ; i+=4) {
+      var r = backgroundData[i];
+      var g = backgroundData[i+1];
+      var b = backgroundData[i+2];
+      var brightness = (3*r+4*g+b)>>>3;
+      if (backgroundAvg[i] === undefined){
+        backgroundAvg[i] = r;
+      } else {
+        backgroundAvg[i] += r;
       }
-      while (video.readyState !== video.HAVE_ENOUGH_DATA){
-         sleep(20);
+      if (backgroundAvg[i+1] === undefined){
+        backgroundAvg[i+1] = g;
+      } else {
+        backgroundAvg[i+1] += g;
       }
-   }
+      if (backgroundAvg[i+2] === undefined){
+        backgroundAvg[i+2] = b;
+      } else {
+        backgroundAvg[i+2] += b;
+      }
+      if (backgroundAvg[i+3] === undefined){
+        backgroundAvg[i+3] = brightness;
+      } else {
+        backgroundAvg[i+3] += brightness;
+      }
+    }
+    while (video.readyState !== video.HAVE_ENOUGH_DATA){
+      sleep(20);
+    }
+  }
 
-   for (var k = 0; k < backgroundAvg.length; k++){
-      backgroundAvg[k] /= numBackFrames;
-   }
+  for (var k = 0; k < backgroundAvg.length; k++){
+    backgroundAvg[k] /= numBackFrames;
+  }
 
-   draw();
+  update();
 }
 
-//draw compares background to foreground 
-function draw() {
+function update(){
 
-   //get data from video stream
-   backContext.drawImage(video,0,0,cw,ch);
+  // var candidate;
+  requestAnimationFrame(update);
 
-   var idata = backContext.getImageData(0,0,cw,ch);
-   var data = idata.data;
+  if (video.readyState === video.HAVE_ENOUGH_DATA){
+    //get the stream as a binary image
+    computeImage();
+    //update and draw data on webpage
+    idata.data = data;
+    context.putImageData(idata,0,0);
 
-   //loop through pixels 
-   //compare color and intensity data of each pixel at location (x,y) to the average background pixel at location (x,y)
-   for(var i = 0; i < data.length; i+=4) {
-      var r = data[i];
-      var g = data[i+1];
-      var b = data[i+2];
+    //use cv.js and handtracking.js library from jcmellado to perform hand detection
+    // candidate = tracker.detect(idata);
+    // var contour = candidate.contour;
+   
+    // if (contour.length > 0){
+    //   context.beginPath();
+    //   context.lineWidth = 3;
+    //   context.strokeStyle = "green";
 
-      var currI = (3*r+4*g+b)>>>3;
-      var backI = backgroundAvg[i+3];
-      var iDiff = Math.abs(currI - backI);
+    //   context.moveTo(contour[0].x, contour[0].y);
+    //   for (i = 0; i < len; ++ i){
+    //   context.lineTo(contour[i].x, contour[i].y);
+    //   }
 
-      var rDiff = Math.abs(r - backgroundAvg[i]);
-      var gDiff = Math.abs(g - backgroundAvg[i+1]);
-      var bDiff = Math.abs(b - backgroundAvg[i+2]);
-      var totalColorDiff = rDiff + gDiff + bDiff;
+    //   context.stroke();
+    //   context.closePath();
+    // }
+  }
+}
 
-      if (iDiff < ithreshold && totalColorDiff < cthreshold) { //background
+//makes a binary image, subtracts background, etc
+function computeImage() {
 
-         //make background pixels black
-         data[i] = 0;
-         data[i+1] = 0;
-         data[i+2] = 0;
+  //get data from video stream
+  backContext.drawImage(video,0,0,cw,ch);
 
-         //add color/intensity data about each pixel for average frame background color/intensity
-         backmeanI += backI;
-         backmeanR += backgroundAvg[i];
-         backmeanG += backgroundAvg[i+1];
-         backmeanB += backgroundAvg[i+2];
-         numback ++;
+  idata = backContext.getImageData(0,0,cw,ch);
+  data = idata.data;
 
-         //update temporalBCG, this pixel has been a background pixel for +1 more frames
-         temporalBCG[i] += 1;
+  //loop through pixels 
+  //compare color and intensity data of each pixel at location (x,y) to the average background pixel at location (x,y)
+  for(var i = 0; i < data.length; i+=4) {
+    var r = data[i];
+    var g = data[i+1];
+    var b = data[i+2];
 
-/****** UPDATING BACKGROUND MODEL   ******/
-         //can comment this section out to make script faster
-         //you can ignore this gnarly math stuff, most of it is just constants have had been empirically determined
-         //if you really wanna understand it, read the paper referenced above
-         var cbg = Math.min(150,temporalBCG[i]);
+    var currI = (3*r+4*g+b)>>>3;
+    var backI = backgroundAvg[i+3];
+    var iDiff = Math.abs(currI - backI);
 
-         //a1 weights the probability of the background pixel based on how high the difference in intensity is;         
-         var exp1 = ((-5) * Math.pow(iDiff,2))/(2 * ithreshold);
-         var a1 = Math.pow(Math.E, exp1);
-         
-         //a2 weights the probability of the background pixel based on how long the background pixel has been a background pixel;
-         var exp2 = -Math.pow((150 - cbg),2)/450;
-         var a2 = Math.pow(Math.E, exp2);
-         
-         //weight intensity probability and temporal probability, you can change the weights tho as long as w1 + w2 <= 1
-         var alpha = (0.75 * a1) + (0.25 * a2);
-         backgroundAvg[i+3] = (alpha * currI) + ((1 - alpha) * backI);
-/******   END UPDATING BACKGROUND MODEL   ******/
+    var rDiff = Math.abs(r - backgroundAvg[i]);
+    var gDiff = Math.abs(g - backgroundAvg[i+1]);
+    var bDiff = Math.abs(b - backgroundAvg[i+2]);
+    var totalColorDiff = rDiff + gDiff + bDiff;
+
+    if (iDiff < ithreshold && totalColorDiff < cthreshold) { //background
+
+      //make background pixels black
+      data[i] = 0;
+      data[i+1] = 0;
+      data[i+2] = 0;
+
+      //add color/intensity data about each pixel for average frame background color/intensity
+      backmeanI += backI;
+      backmeanR += backgroundAvg[i];
+      backmeanG += backgroundAvg[i+1];
+      backmeanB += backgroundAvg[i+2];
+      numback ++;
+
+      //update temporalBCG, this pixel has been a background pixel for +1 more frames
+      temporalBCG[i] += 1;
+
+      //to make things faster, dont call updateBackgroundModel
+      updateBackgroundModel(currI, backI, i);
 
 
-      } else { //foreground
-         //make foreground pixels white
-         data[i] = 255;
-         data[i+1] = 255;
-         data[i+2] = 255;
+    } else { //foreground
+      //make foreground pixels white
+      data[i] = 255;
+      data[i+1] = 255;
+      data[i+2] = 255;
 
-         //add color/intensity data about each pixel for average frame foreground color/intensity
-         foremeanI += currI;
-         foremeanR += r;
-         foremeanG += g;
-         foremeanB += b;            
-         numfore ++;
+      //add color/intensity data about each pixel for average frame foreground color/intensity
+      foremeanI += currI;
+      foremeanR += r;
+      foremeanG += g;
+      foremeanB += b;            
+      numfore ++;
 
-         //reset temporalBCG, this pixel has been a background pixel for 0 frames (its in the foreground now)
-         temporalBCG[i] = 0;
-      }
-   }
+      //reset temporalBCG, this pixel has been a background pixel for 0 frames (its in the foreground now)
+      temporalBCG[i] = 0;
+    }
+  }
 
-   //adapt the intensity threshold
-   ithreshold = ((foremeanI/numfore) + (backmeanI/numback))/2;
+  //adapt the intensity threshold
+  ithreshold = ((foremeanI/numfore) + (backmeanI/numback))/2;
 
-   //adapt the color threshold
-   var redMid = ((foremeanR/numfore) + (backmeanR/numback))/2;
-   var greenMid = ((foremeanG/numfore) + (backmeanG/numback))/2;
-   var blueMid = ((foremeanB/numfore) + (backmeanB/numback))/2;
-   cthreshold = Math.min(redMid, blueMid, greenMid);
+  //adapt the color threshold
+  var redMid = ((foremeanR/numfore) + (backmeanR/numback))/2;
+  var greenMid = ((foremeanG/numfore) + (backmeanG/numback))/2;
+  var blueMid = ((foremeanB/numfore) + (backmeanB/numback))/2;
+  cthreshold = Math.min(redMid, blueMid, greenMid);
 
-   //update and draw data on webpage
-   idata.data = data;
-   context.putImageData(idata,0,0);
+}
 
-   //call this function for every frame
-   setTimeout(draw);
+// you can ignore this gnarly math stuff, most of it is just constants have had been empirically determined
+// if you really wanna understand it, read the paper referenced above
+function updateBackgroundModel(currI, backI, i){
+  var iDiff = Math.abs(currI - backI);
+  var cbg = Math.min(150,temporalBCG[i]);
+
+  //a1 weights the probability of the background pixel based on how high the difference in intensity is;         
+  var exp1 = ((-5) * Math.pow(iDiff,2))/(2 * ithreshold);
+  var a1 = Math.pow(Math.E, exp1);
+  
+  //a2 weights the probability of the background pixel based on how long the background pixel has been a background pixel;
+  var exp2 = -Math.pow((150 - cbg),2)/450;
+  var a2 = Math.pow(Math.E, exp2);
+  
+  //weight intensity probability and temporal probability, you can change the weights tho as long as w1 + w2 <= 1
+  var alpha = (0.75 * a1) + (0.25 * a2);
+  backgroundAvg[i+3] = (alpha * currI) + ((1 - alpha) * backI);
 }
